2e12dcff548a423a10ff4d0d7923eb01f26126be
Yang Li-Yi <joseph.lyyang@gmail.com>
2015-09-17 12:21:40 +0800

    Add create function

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,25 @@
 class MoviesController < ApplicationController
 
+  #GET movies
   def index
   @movies = Movie.all
   end
 
+  #GET movies/new
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+  @movie = Movie.new(movie_params)
+  @movie.save
+
+  redirect_to movies_url
+  end
+
+  private
+
+  def movie_params
+  params.require(:movie).permit(:name, :description)
+  end
 end
