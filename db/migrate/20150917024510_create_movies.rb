e94cb9531a4e8aeac9cdfeba0432f9ed30dbbb32
Yang Li-Yi <joseph.lyyang@gmail.com>
2015-09-17 11:03:34 +0800

    Add movie table

---
+++ db/migrate/20150917024510_create_movies.rb
@@ -0,0 +1,10 @@
+class CreateMovies < ActiveRecord::Migration
+  def change
+    create_table :movies do |t|
+      t.string :name
+      t.text :description
+      t.integer :ranking
+      t.timestamps null: false
+    end
+  end
+end
